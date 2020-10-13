638c1030a41a572106b43b0f791fca81a699db3c
henryhansollee <henryhansollee@gmail.com>
2020-10-13 22:53:06 +0900

    Install and apply react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// Router
+import { Route } from 'react-router-dom';
+
+// Accounts
+import LoginPage from './pages/accounts/LoginPage';
+import SignupPage from './pages/accounts/SignupPage';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* Accounts */}
+      <Route component={LoginPage} path="/login" />
+      <Route component={SignupPage} path="/signup" />
+    </>
   );
-}
+};
 
 export default App;
