aa9add463d11c1de9f9bc16eb2f9e5754b86a9ec
이한솔 <henryhansollee@gmail.com>
2020-10-14 21:47:02 +0900

    Install Material-UI

--- frontend/src/pages/base/MainPage.js
+++ frontend/src/pages/base/MainPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeaderComp from '../../components/base/HeaderComp';
 
 const MainPage = () => {
   return (
     <div>
+      <HeaderComp />
       <h1>메인 페이지</h1>
     </div>
   );
