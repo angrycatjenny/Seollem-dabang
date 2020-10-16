98859893547a5c1201fb436ce7eca5807b4e9dfc
이한솔 <henryhansollee@gmail.com>
2020-10-16 16:02:57 +0900

    Check signup and login with django backend

--- frontend/src/pages/accounts/LoginPage.js
+++ frontend/src/pages/accounts/LoginPage.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react';
 import axios from '../../../node_modules/axios/index';
 
 const LoginPage = () => {
+  const [ username, setUsername ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
 
+  const setUsernameText = e => {
+    setUsername(e.target.value);
+  }
   const setEmailText = e => {
     setEmail(e.target.value);
   };
@@ -13,11 +17,14 @@ const LoginPage = () => {
   };
 
   const sendLoginData = e => {
-    e.preventDefault();
-    const loginData = [email, password]
+    e.preventDefault()
+    const loginData = {username, email, password}
     console.log(loginData, '로그인 정보')
-    axios.post('/login', loginData)
-      .then(console.log('로그인 성공'))
+    axios.post('/rest-auth/login/', loginData)
+      .then((response) => {
+        console.log(response, '토큰이 오겠지?')
+        console.log('로그인 성공')
+      })
       .catch((error) => console.log(error))
   };
 
@@ -26,6 +33,7 @@ const LoginPage = () => {
       <h1>로그인</h1>
       <div className="w-25">
         <form onSubmit={sendLoginData} className="d-flex flex-column">
+          <input placeholder="이름" username={username} onChange={setUsernameText} />
           <input placeholder="이메일" email={email} onChange={setEmailText} />
           <input placeholder="비밀번호" password={password} onChange={setPasswordText} />
           <button type="submit">로그인</button>
