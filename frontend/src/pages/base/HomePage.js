995f406f31292dec4ba2ab26444426bbbca2a48d
이한솔 <henryhansollee@gmail.com>
2020-10-18 15:10:11 +0900

    Add links at home

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -4,6 +4,10 @@ const HomePage = () => {
   return (
     <div>
       <h1>홈 페이지</h1>
+      <div className="d-flex flex-column w-75">
+        <a href="/login" className="btn btn-primary">로그인</a>
+        <a href="/signup" className="btn btn-secondary">회원가입</a>
+      </div>
     </div>
   );
 };
