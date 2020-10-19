744211aa0f61358cb2bfdaef0298eeef2d52d2e0
이한솔 <henryhansollee@gmail.com>
2020-10-19 12:43:57 +0900

    Change header UI

--- frontend/src/components/base/HeaderComp.js
+++ frontend/src/components/base/HeaderComp.js
@@ -1,42 +1,22 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import HeaderLogo from '../../assets/logos/header-logo.png'
-import styled from 'styled-components';
+import { Navbar } from 'react-bootstrap';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-const HeaderLogoImg = styled.img`
-  width: 14%;
-  margin-right: 3%;
-`;
-
-const appBarStyled = {
-  background: 'black'
-};
-
-export default function ButtonAppBar() {
-  const classes = useStyles();
-
+const HeaderComp = () => {
   return (
-    <div className={classes.root}>
-      <AppBar position="static" style={appBarStyled}>
-        <Toolbar>
-          <HeaderLogoImg src={HeaderLogo} />
-          <Typography variant="h6" className={classes.title}>
-            프로젝트 이름
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#">
+        <img
+          alt=""
+          src={HeaderLogo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{' '}
+        프로젝트 이름
+      </Navbar.Brand>
+    </Navbar>
   );
 }
+
+export default HeaderComp;
