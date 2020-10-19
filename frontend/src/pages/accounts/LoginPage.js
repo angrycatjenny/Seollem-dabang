e06071c90c7e6f24576757b3f2ee992123db1fb0
이한솔 <henryhansollee@gmail.com>
2020-10-19 16:23:22 +0900

    Create additional inputs

--- frontend/src/pages/accounts/LoginPage.js
+++ frontend/src/pages/accounts/LoginPage.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
 const LoginPage = ({ history }) => {
+
   // 기본 정보
   const [ username, setUsername ] = useState('');             // 이름
   const [ email, setEmail ] = useState('');                   // 이메일
@@ -27,15 +28,25 @@ const LoginPage = ({ history }) => {
   // 토큰
   const [cookies, setCookie] = useCookies(['access-token']);  // 토큰
 
-  const setUsernameText = e => {
-    setUsername(e.target.value);
-  }
-  const setEmailText = e => {
-    setEmail(e.target.value);
-  };
-  const setPasswordText = e => {
-    setPassword(e.target.value);
-  };
+  // 기본 정보
+  const setUsernameText = e => {setUsername(e.target.value)};       // 이름
+  const setEmailText = e => {setEmail(e.target.value)};             // 이메일
+  const setPasswordText = e => {setPassword(e.target.value)};       // 비밀번호
+
+  // 추가 정보
+  const setImageText = e => {setImage(e.target.value)};             // 프로필 사진
+  const setNicknameText = e => {setNickname(e.target.value)};       // 닉네임
+  const setUniversityText = e => {setUniversity(e.target.value)};   // 학교
+  const setMajorText = e => {setMajor(e.target.value)};             // 전공
+  const setJobText = e => {setJob(e.target.value)};                 // 직업
+  const setWorkText = e => {setWork(e.target.value)};               // 직장
+  const setBirthText = e => {setBirth(e.target.value)};             // 생년월일
+  const setHeightText = e => {setHeight(e.target.value)};           // 키
+  const setWeightText = e => {setWeight(e.target.value)};           // 체형
+  const setPersonalityText = e => {setPersonality(e.target.value)}; // 성격
+  const setBloodTypeText = e => {setBloodType(e.target.value)};     // 혈액형
+  const setSmokingText = e => {setSmoking(e.target.value)};         // 흡연여부
+  const setReligionText = e => {setReligion(e.target.value)};       // 종교
 
   const sendLoginData = e => {
     e.preventDefault()
@@ -56,9 +67,27 @@ const LoginPage = ({ history }) => {
       <h1>로그인</h1>
       <div className="w-25">
         <form onSubmit={sendLoginData} className="d-flex flex-column">
+
+          {/* 기본정보 */}
           <input placeholder="이름" username={username} onChange={setUsernameText} />
           <input placeholder="이메일" email={email} onChange={setEmailText} />
           <input placeholder="비밀번호" password={password} onChange={setPasswordText} />
+
+          {/* 추가정보 */}
+          <input placeholder="프로필 사진" image={image} onChange={setImageText} />
+          <input placeholder="닉네임" nickname={nickname} onChange={setNicknameText} />
+          <input placeholder="학교" university={university} onChange={setUniversityText} />
+          <input placeholder="전공" major={major} onChange={setMajorText} />
+          <input placeholder="직업" job={job} onChange={setJobText} />
+          <input placeholder="직장" work={work} onChange={setWorkText} />
+          <input placeholder="생년월일" birth={birth} onChange={setBirthText} />
+          <input placeholder="키" height={height} onChange={setHeightText} />
+          <input placeholder="체형" weight={weight} onChange={setWeightText} />
+          <input placeholder="성격" personality={personality} onChange={setPersonalityText} />
+          <input placeholder="혈액형" bloodType={bloodType} onChange={setBloodTypeText} />
+          <input placeholder="흡연여부" smoking={smoking} onChange={setSmokingText} />
+          <input placeholder="종교" religion={religion} onChange={setReligionText} />
+
           <button type="submit">로그인</button>
         </form>
       </div>
