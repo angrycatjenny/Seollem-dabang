e3393b41f41b415235095572bda3522b9f56cce5
임기람 <rlfka@naver.com>
2020-10-19 22:40:58 +0900

    change components name & add if condition

--- frontend/src/pages/exam/ExamCreatePage.js
+++ frontend/src/pages/exam/ExamCreatePage.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import HeaderComp from '../../components/base/HeaderComp';
+import FooterComp from '../../components/base/FooterComp';
 import axios from 'axios';
 
-//로그인 여부에 따른 글쓰기 허용은 나중에
-const ArticleCreatePage = () => {
+//로그인 여부에 따른 시험지 작성 허용
+//데이터 변수 하나 설정하고 조건부 렌더링
+//null이면 질문 개수 설정, !null이면 질문create
+const ExamCreatePage = () => {
     const [ title, setTitle ] = useState('');
     const [ content, setContent ] = useState('');
 
@@ -38,8 +41,9 @@ const ArticleCreatePage = () => {
             </form>
             <Link to="/"><button>취소</button></Link>
         </div>
+        <FooterComp />
       </>
     );
   };
   
-  export default ArticleCreatePage;
+  export default ExamCreatePage;
---
+++ frontend/src/pages/exam/ExamDetailPage.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import HeaderComp from '../../components/base/HeaderComp';
+import FooterComp from '../../components/base/FooterComp';
+
+// import axios from 'axios';
+
+//로그인 여부에 따른 글쓰기 허용은 나중에
+const ExamDetailPage = () => {
+    // const [exam, setExam] = useState([]);
+    const [isExam, setIsExam] = useState(false);
+
+    // useEffect(() => {
+    //     axios.get('/exam/')
+    //     .then((res) => {
+    //         console.log(res,'옴?')
+    //         setExam(res.data);
+    //         if (res.data) {
+    //            setIsExam(true)
+    //      }
+    //     })
+    //     .catch((error) => console.log(error));
+    //   }, []);
+
+    // const { questions } = questions;
+
+    return (
+      <>
+        <HeaderComp />
+        {/* {questions.map((question) => {
+          return (
+            <>
+            <h4>{question.title}</h4>
+            <h5>{question.content}</h5>
+            </>
+            )
+          })} */}
+        {/* 푸터에 홈버튼 있으니까 필요x */}
+        {/* <Link to="/"><button>홈으로</button></Link> */}
+        {isExam ? (
+          <>
+        <Link to="/exam/update"><button>시험지 수정</button></Link>
+        <button>시험지 삭제</button>
+        </>
+      ) : (
+        <Link to="/exam/create"><button>시험지+</button></Link>
+      )}
+        <FooterComp/>
+      </>
+    );
+  };
+  //글쓰기로 가는 주소 수정 필요. 
+  export default ExamDetailPage;
