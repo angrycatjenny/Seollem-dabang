1dd429111201303d0642fbe8864a91f17d96e06c
이한솔 <henryhansollee@gmail.com>
2020-10-21 19:54:07 +0900

    Change bootstrap -> css

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import styled from 'styled-components';
 import './HomePage.css';
-
-const HomeTemplate = styled.div`
-  margin-top: 40%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 
 const HomePage = () => {
   return (
-    <HomeTemplate>
+    <div className="home-template">
       <div className="neon">설레임</div>
       <div className="flux">방정식</div>
-      <div className="d-flex flex-column w-75 mt-5">
-        <a href="/login" className="btn btn-primary">로그인</a>
-        <a href="/signup" className="btn btn-secondary mt-2">회원가입</a>
+      <div className="buttons">
+        <a href="/login"><button className="login-button">로그인</button></a>
+        <a href="/signup"><button className="signup-button">회원가입</button></a>
       </div>
-    </HomeTemplate>
+    </div>
   );
 };
 
