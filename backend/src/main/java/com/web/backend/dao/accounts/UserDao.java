e74752a74e8b6ddf8bf935ca3a818f0339c7747a
최보근 <bogun11904@naver.com>
2020-10-22 00:59:43 +0900

    Set signup for user

--- backend/src/main/java/com/web/backend/dao/accounts/UserDao.java
+++ backend/src/main/java/com/web/backend/dao/accounts/UserDao.java
@@ -1,4 +1,20 @@
 package com.web.backend.dao.accounts;
 
-public class UserDao {
+import com.web.backend.model.accounts.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserDao extends JpaRepository<User, Long> {
+
+    Optional<User> findByUserEmail(String userEmail);
+
+    List<User> findByUserIdIn(List<Long> userIds);
+
+    Boolean existsByUserEmail(String userEmail);
+
+    Boolean existsByUserNickname(String userNickname);
 }
--- backend/src/main/java/com/web/backend/model/accounts/User.java
+++ backend/src/main/java/com/web/backend/model/accounts/User.java
@@ -1,4 +1,105 @@
 package com.web.backend.model.accounts;
 
-public class User {
+import com.web.backend.model.audit.DateAudit;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = { "userEmail" }), @UniqueConstraint(columnNames = { "userNickname" })})
+public class User extends DateAudit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 40)
+    private String userEmail;
+
+    @NotBlank
+    @Size(max = 100)
+    private String userPassword;
+
+    @NotBlank
+    @Size(max = 20)
+    private String userNickname;
+
+    @NotBlank
+    @Size(max = 10)
+    private String userLocation;
+
+    @NotBlank
+    private int userGender;
+
+    @NotBlank
+    private int userAge;
+
+    public User() { }
+
+    public User(String userEmail, String userPassword, String userNickname, String userLocation, int userGender, int userAge) {
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
+        this.userNickname = userNickname;
+        this.userLocation = userLocation;
+        this.userGender = userGender;
+        this.userAge = userAge;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserNickname() {
+        return userNickname;
+    }
+
+    public void setUserNickname(String userNickname) {
+        this.userNickname = userNickname;
+    }
+
+    public String getUserLocation() {
+        return userLocation;
+    }
+
+    public void setUserLocation(String userLocation) {
+        this.userLocation = userLocation;
+    }
+
+    public int getUserGender() {
+        return userGender;
+    }
+
+    public void setUserGender(int userGender) {
+        this.userGender =userGender;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
 }
