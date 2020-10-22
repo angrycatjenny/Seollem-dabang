8cf5abb116fa56fe45de375cb827b9851ded26f3
이한솔 <henryhansollee@gmail.com>
2020-10-22 13:43:06 +0900

    Update signup layout

--- frontend/src/pages/accounts/SignupPage.js
+++ frontend/src/pages/accounts/SignupPage.js
@@ -4,7 +4,6 @@ import axios from 'axios';
 import './signupPage.css';
 
 // 지역
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -22,8 +21,6 @@ const MenuProps = {
     },
   },
 };
-
-// 지역
 const locations = [ '서울', '경기', '인천', '강원', '대전', '세종', '충남', '충북', '부산', '울산', '경남', '경북', '대구', '전남', '전북', '제주', '광주',];
 
 const SignupPage = ({ history }) => {
@@ -66,41 +63,41 @@ const SignupPage = ({ history }) => {
   return (
     <div>
       <HeaderComp />
-      <h1>회원가입</h1>
-      <div className="w-25">
-        <form onSubmit={sendSignupData} className="d-flex flex-column">
-          <input placeholder="이메일" email={email} onChange={setEmailText} />
-          <input placeholder="비밀번호" password={password} onChange={setPasswordText} />
-          <input placeholder="비밀번호 확인" passwordConf={passwordConf} onChange={setPasswordConfText} />
-          <input placeholder="닉네임" nickname={nickname} onChange={setNicknameText} />
-          <input placeholder="성별" gender={gender} onChange={setGenderText} />
-          <input placeholder="나이" age={age} onChange={setAgeText} />
+      <h1 className="signup-logo">회원가입</h1>
+      <form onSubmit={sendSignupData} className="signup-form">
+        <input placeholder="이메일" email={email} onChange={setEmailText} />
+        <input placeholder="비밀번호" password={password} onChange={setPasswordText} />
+        <input placeholder="비밀번호 확인" passwordConf={passwordConf} onChange={setPasswordConfText} />
+        <input placeholder="닉네임" nickname={nickname} onChange={setNicknameText} />
+        <input placeholder="성별" gender={gender} onChange={setGenderText} />
+        <input placeholder="나이" age={age} onChange={setAgeText} />
 
-          <FormControl>
-            <InputLabel id="demo-mutiple-name-label">지역</InputLabel>
-            <Select
-              labelId="demo-mutiple-name-label"
-              id="demo-mutiple-name"
-              value={location}
-              onChange={setLocationText}
-              input={<Input />}
-              MenuProps={MenuProps}
-            >
-              {locations.map((location) => (
-                <MenuItem key={location} value={location}>
-                  {location}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <FormControl>
+          <InputLabel id="demo-mutiple-name-label">지역</InputLabel>
+          <Select
+            labelId="demo-mutiple-name-label"
+            id="demo-mutiple-name"
+            value={location}
+            onChange={setLocationText}
+            input={<Input />}
+            MenuProps={MenuProps}
+          >
+            {locations.map((location) => (
+              <MenuItem key={location} value={location}>
+                {location}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
 
-          <input placeholder="프로필 사진" image={image} onChange={setImageText} />
-          <input placeholder="사용자 소개 음성" voice={voice} onChange={setVoiceText} />
-          <button type="submit">회원가입</button>
-        </form>
-      </div>
-      <small>이미 회원이신가요?</small>
-      <a href="/login">로그인</a>
+        <input placeholder="프로필 사진" image={image} onChange={setImageText} />
+        <input placeholder="사용자 소개 음성" voice={voice} onChange={setVoiceText} />
+        <div className="signup-footer">
+          <small>이미 회원이신가요?</small>
+          <a href="/login">로그인</a>
+        </div>
+        <button className="signup-button" type="submit">회원가입</button>
+      </form>
     </div>
   );
 };
