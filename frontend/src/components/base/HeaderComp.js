77e4ace901b006dabc790dcc36d079dda31b9d85
이한솔 <henryhansollee@gmail.com>
2020-10-25 23:28:51 +0900

    Create logout

--- frontend/src/components/base/HeaderComp.js
+++ frontend/src/components/base/HeaderComp.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './HeaderComp.css';
 import ProfileImage from '../../assets/profile/profile-image.png';
+import { useCookies } from 'react-cookie';
 
 const HeaderComp = () => {
+  const [ cookies, setCookie, removeCookie ] = useCookies(['access-token']);
+  
+  const logout = e => {
+    console.log('쿠키삭제')
+    removeCookie('access-token');
+  }
+
   return (
     <div className="navbar">
       <div>
         <img className="profile-image" src={ProfileImage} />
         <small>이한솔</small>
       </div>
-      <small>로그아웃</small>
+      <button onClick={logout}>로그아웃</button>
     </div>
   );
 }
