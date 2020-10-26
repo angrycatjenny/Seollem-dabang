32d1d04de62b303b7b521caa0f400dbf69e791bf
임기람 <rlfka@naver.com>
2020-10-26 16:50:38 +0900

    change button

--- frontend/src/pages/question/QuestionListPage.js
+++ frontend/src/pages/question/QuestionListPage.js
@@ -1,11 +1,12 @@
 import React, { useState,useEffect } from 'react';
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import HeaderComp from '../../components/base/HeaderComp';
 import FooterComp from '../../components/base/FooterComp';
 import axios from 'axios';
 import { useCookies } from 'react-cookie';
 
 const QuestionListPage = () => {
+  const [isExam, setIsExam] = useState(false);
   const [ questionList, setQuestionList ] = useState({
       questions:[],
   });
@@ -17,10 +18,13 @@ const QuestionListPage = () => {
   const getQuestions = () => {
       axios.get(`/question`,config)
       .then((response) => {
-        console.log(response,'질문들 옴?')
-        setQuestionList({
-          questions: response.data
-        })
+        if(response.data){
+          console.log(response,'질문들 옴?')
+          setQuestionList({
+            questions: response.data
+          })
+          setIsExam(true)
+        }
       })
       .catch((err) => {
         console.log(err)
@@ -34,9 +38,21 @@ const QuestionListPage = () => {
   return (
     <>
       <HeaderComp/>
-        {questions.map((question) => (
-                {question}
-              ))}
+        {isExam ? (
+          <>
+            {questions.map((question) => {
+              return (
+                <>
+                <h4>{question}</h4>
+                <button>시험지 수정</button>
+                <button>시험지 삭제</button>
+                </>
+                )
+              })}
+          </>
+        ) : (
+          <Link to="/question/create"><button>시험지+</button></Link>
+        )}
       <FooterComp />
     </>
   );
