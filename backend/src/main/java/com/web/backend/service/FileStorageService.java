1e1bf0853bca6f8dbc3c225cdcfb231b556828ad
최보근 <bogun11904@naver.com>
2020-10-26 17:33:27 +0900

    Create FileStorageService.java

---
+++ backend/src/main/java/com/web/backend/service/FileStorageService.java
@@ -0,0 +1,65 @@
+package com.web.backend.service;
+
+import com.web.backend.exception.FileNotFoundException;
+import com.web.backend.exception.FileStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class FileStorageService {
+
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public FileStorageService(FileStorageConfig fileStorageConfig) {
+        this.fileStorageLocation = Paths.get(fileStorageConfig.getUploadDir()).toAbsolutePath().normalize();
+
+        try {
+            Files.createDirectories(this.fileStorageLocation);
+        }catch (Exception e) {
+            throw new FileStorageException("Could not create the directory where the uploaded file will be stored.", e);
+        }
+    }
+
+    public String storeFile(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            if(fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
+
+            return fileName;
+        }catch (IOException e) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if(resource.exists()) {
+                return resource;
+            }else {
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        }catch (MalformedURLException e) {
+            throw new FileNotFoundException("File not found " + fileName, e)
+        }
+    }
+}
