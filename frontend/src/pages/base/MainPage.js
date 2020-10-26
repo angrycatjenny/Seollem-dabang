710719e5987958640b2b0a655fc2e18f86d76306
김도연 <kdymay93@naver.com>
2020-10-26 23:43:21 +0900

    feture/add otherRecommend, resolves S03P31B103-161

--- frontend/src/pages/base/MainPage.js
+++ frontend/src/pages/base/MainPage.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import HeaderComp from '../../components/base/HeaderComp';
 import FooterComp from '../../components/base/FooterComp';
+import KeywordRecommend from '../../components/recommend/KeywordRecommend';
+import OtherRecommend from '../../components/recommend/OtherRecommend';
 
 const MainPage = () => {
   return (
     <div>
       <HeaderComp />
       <h1>메인 페이지</h1>
+      <KeywordRecommend />
+      <OtherRecommend />
       <FooterComp />
     </div>
   );
