7da4e89dfd7f5adab33908a2cf6cf8de4685db2f
최보근 <bogun11904@naver.com>
2020-10-26 14:07:33 +0900

    Modify urls

--- backend/src/main/java/com/web/backend/controller/accounts/UserController.java
+++ backend/src/main/java/com/web/backend/controller/accounts/UserController.java
@@ -17,7 +17,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,7 +24,6 @@ import javax.validation.Valid;
 import java.net.URI;
 
 @RestController
-@RequestMapping("/api/auth")
 public class UserController {
 
     @Autowired
