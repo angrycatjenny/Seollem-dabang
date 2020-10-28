5ea91a663c4b942692a402f15b35c4da11726c26
차종욱 <hedge0207@gmail.com>
2020-10-28 14:10:33 +0900

    rec by profile com

--- backend/src/main/java/com/web/backend/controller/question/QuestionController.java
+++ backend/src/main/java/com/web/backend/controller/question/QuestionController.java
@@ -17,7 +17,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
