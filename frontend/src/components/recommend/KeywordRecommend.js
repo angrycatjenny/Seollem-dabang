36ee949e9680a1c4cd4a533e5c6a71971ba839a9
김도연 <kdymay93@naver.com>
2020-10-28 13:21:15 +0900

    feature/recommend edit

--- frontend/src/components/recommend/KeywordRecommend.js
+++ frontend/src/components/recommend/KeywordRecommend.js
@@ -3,9 +3,45 @@ import Carousel from 'nuka-carousel';
 import './KeywordRecommend.css';
 import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
 import ChevronLeftRoundedIcon from '@material-ui/icons/ChevronLeftRounded';
-import KeywordRecommendItem from './KeywordRecommendItem.js';
-const arr = [1,2,3,4];
-const gender = 0
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+import Button from '@material-ui/core/Button';
+
+import ManImage5 from '../../assets/mainImg/png/남성5.png';
+import ManImage6 from '../../assets/mainImg/png/남성6.png';
+import ManImage7 from '../../assets/mainImg/png/남성7.png';
+import ManImage8 from '../../assets/mainImg/png/남성8.png';
+
+import womenImage5 from '../../assets/mainImg/png/여성5.png';
+import womenImage6 from '../../assets/mainImg/png/여성6.png';
+import womenImage7 from '../../assets/mainImg/png/여성7.png';
+import womenImage8 from '../../assets/mainImg/png/여성8.png';
+
+const imgData = [ManImage5, ManImage6, ManImage7, ManImage8]
+const tileData = [
+    {
+        title: 'Image',
+        author: 'author'
+    },
+    {
+        title: 'Image',
+        author: 'author'
+    },
+    {
+        title: 'Image',
+        author: 'author'
+    },
+    {
+        title: 'Image',
+        author: 'author'
+    },
+]
+for (let i = 0; i < tileData.length; i++) {
+    tileData[i].img = imgData[i]
+}
 const KeywordRecommend = () => {
     return (
         <Carousel
@@ -17,8 +53,24 @@ const KeywordRecommend = () => {
             )}
             classname="carusel-outline"
         >
-            {arr.map((value,index) => (
-                <KeywordRecommendItem key={index} value={value} gender2={gender}/>
+            {tileData.map((tile) => (
+                <GridList cellHeight={300}>
+                    <GridListTile>
+                        <Button>
+                            <img src={tile.img} alt="img" />
+                        </Button>
+                        <GridListTileBar
+                            title='title'
+                            subtitle='by'
+                            actionIcon={
+                                <IconButton>
+                                    <RecordVoiceOverIcon color="secondary" />
+                                </IconButton>
+                            }
+                        />
+                    </GridListTile>
+                </GridList>
+
             ))}
         </Carousel>
     );
@@ -30,9 +82,27 @@ export default KeywordRecommend;
 // import './KeywordRecommend.css';
 // import ChevronRightRoundedIcon from '@material-ui/icons/ChevronRightRounded';
 // import ChevronLeftRoundedIcon from '@material-ui/icons/ChevronLeftRounded';
-// import KeywordRecommendItem from './KeywordRecommendItem.js';
+// import GridList from '@material-ui/core/GridList';
+// import GridListTile from '@material-ui/core/GridListTile';
+// import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import IconButton from '@material-ui/core/IconButton';
+// import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+// import Button from '@material-ui/core/Button';
+
+// import ManImage5 from '../../assets/mainImg/png/남성5.png';
+// import ManImage6 from '../../assets/mainImg/png/남성6.png';
+// import ManImage7 from '../../assets/mainImg/png/남성7.png';
+// import ManImage8 from '../../assets/mainImg/png/남성8.png';
+
+// import womenImage5 from '../../assets/mainImg/png/여성5.png';
+// import womenImage6 from '../../assets/mainImg/png/여성6.png';
+// import womenImage7 from '../../assets/mainImg/png/여성7.png';
+// import womenImage8 from '../../assets/mainImg/png/여성8.png';
 // import { useCookies } from 'react-cookie';
 // import axios from 'axios';
+
+// const menImgData = [ManImage5, ManImage6, ManImage7, ManImage8]
+// const womenImgData = [womenImage5, womenImage6, womenImage7, womenImage8]
 
 // const [gender, setGender] = React.useState();
 
@@ -40,22 +110,31 @@ export default KeywordRecommend;
 // const [cookies] = useCookies(['accessToken']);
 
 // const axiosConfig = {
-//   headers: { token: cookies.accessToken } 
+//     headers: { token: cookies.accessToken }
 // }
 
 // const getData = () => {
-//   axios.get(`/profile/data`,axiosConfig)
-//   .then((response) => {
-//     setTileData(response.data)
-//     setGender(response.gender)
-//   })
-//   .catch((err) => {
-//     console.log(err)
-//     })
+//     axios.get(`/profile/data`, axiosConfig)
+//         .then((response) => {
+//             setTileData(response.data)
+//             setGender(response.gender)
+//         })
+//         .catch((err) => {
+//             console.log(err)
+//         })
 // }
-// React.useEffect(()=>{
-//   getData()
+// React.useEffect(() => {
+//     getData()
 // })
+// if (gender === 0) {
+//     for (let i = 0; i < tileData.length; i++) {
+//         tileData[i].img = menImgData[i]
+//     }
+// } else {
+//     for (let i = 0; i < tileData.length; i++) {
+//         tileData[i].img = womenImgData[i]
+//     }
+// }
 
 // const KeywordRecommend = () => {
 //     return (
@@ -68,8 +147,24 @@ export default KeywordRecommend;
 //             )}
 //             classname="carusel-outline"
 //         >
-//             {tileData.map((tile,index) => (
-//                 <KeywordRecommendItem key={index} profile={tile} gender2={gender}/>
+//             {tileData.map((tile) => (
+//                 <GridList cellHeight={300}>
+//                     <GridListTile>
+//                         <Button>
+//                             <img src={tile.img} alt="img" />
+//                         </Button>
+//                         <GridListTileBar
+//                             title={tile.name}
+//                             subtitle={<span>{tile.region} and {tile.age}세</span>}
+//                             actionIcon={
+//                                 <IconButton>
+//                                     <RecordVoiceOverIcon color="secondary" />
+//                                 </IconButton>
+//                             }
+//                         />
+//                     </GridListTile>
+//                 </GridList>
+
 //             ))}
 //         </Carousel>
 //     );
--- frontend/src/components/recommend/OtherRecommend.js
+++ frontend/src/components/recommend/OtherRecommend.js
@@ -3,36 +3,36 @@ import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import IconButton from '@material-ui/core/IconButton';
-import Image1 from '../../assets/mainImg/png/남성1.png';
-import Image2 from '../../assets/mainImg/png/남성2.png';
-import Image3 from '../../assets/mainImg/png/남성3.png';
-import Image4 from '../../assets/mainImg/png/남성4.png';
 import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 import Button from '@material-ui/core/Button';
+import menImage1 from '../../assets/mainImg/png/남성1.png';
+import menImage2 from '../../assets/mainImg/png/남성2.png';
+import menImage3 from '../../assets/mainImg/png/남성3.png';
+import menImage4 from '../../assets/mainImg/png/남성4.png';
 import './OtherRecommend.css';
+
+const imgData = [menImage1,menImage2,menImage3,menImage4]
 const tileData = [
     {
-        img: Image1,
         title: 'Image',
         author: 'author'
     },
     {
-        img: Image2,
         title: 'Image',
         author: 'author'
     },
     {
-        img: Image3,
         title: 'Image',
         author: 'author'
     },
     {
-        img: Image4,
         title: 'Image',
         author: 'author'
     },
 ]
-
+for(let i=0; i < tileData.length; i++){
+  tileData[i].img = imgData[i]
+}
 const OtherRecommend = () => { 
     return (
         <div className="other-root">
@@ -64,11 +64,26 @@ export default OtherRecommend;
 // 요청 생성 후 음석 재생 및 유저정보 변수에 추가해야 함..
 
 // import React from 'react';
-// import './OtherRecommend.css';
 // import GridList from '@material-ui/core/GridList';
-// import OtherRecommendItem from './OtherRecommendItem.js';
+// import GridListTile from '@material-ui/core/GridListTile';
+// import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import IconButton from '@material-ui/core/IconButton';
+// import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
+// import Button from '@material-ui/core/Button';
+// import menImage1 from '../../assets/mainImg/png/남성1.png';
+// import menImage2 from '../../assets/mainImg/png/남성2.png';
+// import menImage3 from '../../assets/mainImg/png/남성3.png';
+// import menImage4 from '../../assets/mainImg/png/남성4.png';
+
+// import womenImage1 from '../../assets/mainImg/png/여성1.png';
+// import womenImage2 from '../../assets/mainImg/png/여성2.png';
+// import womenImage3 from '../../assets/mainImg/png/여성3.png';
+// import womenImage4 from '../../assets/mainImg/png/여성4.png';
 // import { useCookies } from 'react-cookie';
 // import axios from 'axios';
+
+// const menImgData = [menImage1,menImage2,menImage3,menImage4]
+// const womenImgData = [womenImage1,womenImage2,womenImage3,womenImage4]
 
 // const [gender, setGender] = React.useState();
 
@@ -93,13 +108,36 @@ export default OtherRecommend;
 //   getData()
 // })
 
+// if(gender === 0){
+//   for(let i=0; i < tileData.length; i++){
+//     tileData[i].img = menImgData[i]
+//   }
+// }else{
+//   for(let i=0; i < tileData.length; i++){
+//     tileData[i].img = womenImgData[i]
+//   }
+// }
+
 // const OtherRecommend = () => {
 //   return (
 //     <div className="other-root">
 //       <GridList cellHeight={200} className="other-gridlist">
-//         {tileData.map((tile,index) => (
-//           <OtherRecommendItem key={index} profile={tile} gender2={gender}/>
-//         ))}
+//         {tileData.map((tile) => (
+//             <GridListTile className="other-gridlist-item">
+//             <Button>
+//               <img src={tile.img} alt={tile.name} className="other-imgsize" />
+//             </Button>
+//             <GridListTileBar
+//               title={tile.name}
+//               subtitle={<span>{tile.region} and {tile.age}세</span>}
+//               actionIcon={
+//                 <IconButton className="other-icon">
+//                   <RecordVoiceOverIcon color="secondary" />
+//                 </IconButton>
+//               }
+//             />
+//           </GridListTile>
+//             ))}
 //       </GridList>
 //     </div>
 //   );
