c9dd07c43801de887da2c23718fd2f18b4dd7930
이한솔 <henryhansollee@gmail.com>
2020-10-28 16:02:27 +0900

    Update AnwserCreate

--- frontend/src/pages/answer/AnswerCreatePage.js
+++ frontend/src/pages/answer/AnswerCreatePage.js
@@ -12,16 +12,25 @@ import { useHistory } from "react-router-dom";
 // Cookie
 import { useCookies } from 'react-cookie';
 
+// Material-UI
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+
 const AnswerCreatePage = () => {
   const history = useHistory();
   const [ cookies, setCookie ] = useCookies(['accessToken']);
 
-  const [ questions, setQuestions ] = useState({
-    items: [],
-  });
-  const { items } = questions;
+  const [ questions, setQuestions ] = useState(null);
+  const [ loading, setLoading ] = useState(false);
 
-  const [ answers, setAnswers ] = useState([]);
+  const [value, setValue] = useState('');
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
   const config = {
     headers: {
@@ -29,21 +38,19 @@ const AnswerCreatePage = () => {
     }
   }
   useEffect(() => {
-    axios.get('/question/list', config)
-      .then((response) => {
-        const questionList = response.data.map((question, index) => {
-          return {
-            questionId: question.questionId,
-            content: question.content,
-          };
-        });
-        setQuestions({
-          items: items.concat(questionList),
-        });
-      })
-      .catch((error) => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          '/question/list', config
+        );
+        setQuestions(response.data);
+      } catch (error) {
         console.log(error);
-      });
+      }
+      setLoading(false);
+    };
+    fetchData();
   }, []);
 
   const sendAnswers = e => {
@@ -54,14 +61,27 @@ const AnswerCreatePage = () => {
     .catch()
   }
 
+  if (loading) {
+    return <h1>대기 중...</h1>;
+  }
+  if (!questions) {
+    return null;
+  }
+
   return (
     <div>
       <h1>답변 등록</h1>
-      <div>
-        <p>{questions.content}</p>
-        <button>YES</button>
-        <button>NO</button>
-      </div>
+      {questions.map((question, index) => (
+        <div key={index}>
+          <h1>{question.content}</h1>
+          <FormControl component="fieldset">
+            <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+              <FormControlLabel value="1" control={<Radio />} label="Yes" />
+              <FormControlLabel value="0" control={<Radio />} label="No" />
+            </RadioGroup>
+          </FormControl>
+        </div>
+      ))}
       <button onClick={sendAnswers}>제출하기</button>
     </div>
   )
