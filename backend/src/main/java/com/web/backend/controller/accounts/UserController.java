173b9c84d51b605b8f209791a7cf55ed8be8bc73
차종욱 <hedge0207@gmail.com>
2020-10-30 12:14:49 +0900

    recommend fix

--- backend/src/main/java/com/web/backend/controller/accounts/UserController.java
+++ backend/src/main/java/com/web/backend/controller/accounts/UserController.java
@@ -25,7 +25,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
-import java.lang.reflect.Array;
 import java.net.URI;
 import java.util.*;
 
