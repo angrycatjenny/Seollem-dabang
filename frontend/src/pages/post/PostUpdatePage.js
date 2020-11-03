4eb22a8e280c572bf16b31c02d23654f5971c53d
이한솔 <henryhansollee@gmail.com>
2020-11-03 16:48:20 +0900

    Export default pages

---
+++ frontend/src/pages/post/PostUpdatePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PostUpdatePage.css';
+
+const PostUpdatePage = () => {
+  return (
+    <div>
+      <h1>게시물 수정 페이지</h1>
+    </div>
+  )
+}
+
+export default PostUpdatePage;
