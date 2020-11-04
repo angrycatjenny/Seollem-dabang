41d0303e5fd1a152080e391de8cbf8692ba70105
김도연 <kdymay93@naver.com>
2020-11-04 14:52:29 +0900

    feature/footer-edit, resolves S03P31B103-330

--- frontend/src/components/base/FooterComp.js
+++ frontend/src/components/base/FooterComp.js
@@ -2,26 +2,26 @@ import React from 'react';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import PersonRoundedIcon from '@material-ui/icons/PersonRounded';
+// import FavoriteIcon from '@material-ui/icons/Favorite';
+import SmsIcon from '@material-ui/icons/Sms';
+import RecordVoiceOverIcon from '@material-ui/icons/RecordVoiceOver';
 import AssignmentRoundedIcon from '@material-ui/icons/AssignmentRounded';
 import { useHistory } from "react-router-dom";
 import { useRouteMatch } from 'react-router-dom';
 import './FooterComp.css';
-import { useCookies } from 'react-cookie';
 
 export default function FooterComp() {
   let [value, setValue] = React.useState();
-  const [cookies, setCookie] = useCookies(['accessToken']);
   const history = useHistory();
   const match = useRouteMatch();
+  console.log(match)
   if (match.path ==='/main'){
     value = 0
   }else if (match.path==='/question'||match.path==='/question/create'||match.path==='/question/update'){
     value = 1
   }else if (match.path==='/conversation'||match.path==='/conversation/:conversationId'){
     value = 2
-  }else if (match.path==='/profile' || match.path==='/myprofile/update'|| match.path==='/yourprofile/:userId'){
+  }else if (match.path==='/post/create' || match.path==='/post/list'|| match.path==='/post/update/:postId'){
     value = 3
   }
 
@@ -34,10 +34,10 @@ export default function FooterComp() {
       showLabels
       className="navbar-footer"
     >
-      <BottomNavigationAction onClick={() => history.push('/main')} label="홈"  icon={<HomeRoundedIcon />} />
-      <BottomNavigationAction onClick={() => history.push('/question')} label="모의고사" icon={<AssignmentRoundedIcon />} />
-      <BottomNavigationAction onClick={() => history.push('/conversation')} label="채팅"  icon={<FavoriteIcon />} />
-      <BottomNavigationAction onClick={() => history.push(`/profile`)} label="내정보"  icon={<PersonRoundedIcon />} />
+      <BottomNavigationAction onClick={() => history.push('/main')} label=""  icon={<HomeRoundedIcon />} />
+      <BottomNavigationAction onClick={() => history.push('/question')} label="" icon={<AssignmentRoundedIcon />} />
+      <BottomNavigationAction onClick={() => history.push('/conversation')} label=""  icon={<SmsIcon />} />
+      <BottomNavigationAction onClick={() => history.push(`/post/list`)} label=""  icon={<RecordVoiceOverIcon />} />
     </BottomNavigation>
   );
 }
