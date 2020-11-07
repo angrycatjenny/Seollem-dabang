bad09e3c1012c3fe16b166d0bd55a46f45bc6edb
이한솔 <henryhansollee@gmail.com>
2020-11-07 23:05:15 +0900

    Update home logo

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -5,8 +5,9 @@ import { Link } from 'react-router-dom';
 const HomePage = () => {
   return (
     <div className="home-template">
-      <div className="neon">설레임</div>
-      <div className="flux">방정식</div>
+      <div class="sign">
+        <span class="fast-flicker">설</span>레임<span class="flicker">방</span>정식
+      </div>
       <div className="buttons">
         <Link className="btn btn-light home-login-button" to="/login">로그인</Link>
         <Link className="btn btn-light home-signup-button" to="/signup">회원가입</Link>
