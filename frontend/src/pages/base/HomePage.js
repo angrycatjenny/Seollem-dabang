ac582a97be43106a5d433bca16ff3dd1e89fdf07
김도연 <kdymay93@naver.com>
2020-11-08 16:52:00 +0900

    style/hompage

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -20,7 +20,7 @@ const HomePage = () => (
                 <div className="neon">설레임</div>
                 <div className="flux">방정식</div>
               </div>
-              <div className="Buttons">
+              <div className="buttons">
                 <Link className="btn btn-light home-login-button" to="/login">로그인</Link>
                 <Link className="btn btn-light home-signup-button" to="/signup">회원가입</Link>
               </div>
