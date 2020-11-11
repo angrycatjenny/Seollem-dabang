4078b65eebdaeb633803b6b5bd20061460b96b56
이한솔 <henryhansollee@gmail.com>
2020-11-11 21:15:26 +0900

    Create scroll down button

--- frontend/src/pages/base/HomePage.js
+++ frontend/src/pages/base/HomePage.js
@@ -6,6 +6,9 @@ import HomeSection1 from '../../assets/home/HomeSection1.JPG';
 import HomeSection2 from '../../assets/home/HomeSection2.JPG';
 import HomeSection3 from '../../assets/home/HomeSection3.JPG';
 import HomeSection4 from '../../assets/home/HomeSection4.JPG';
+import ScrollDown from '../../assets/home/ScrollDown.png';
+import ScrollStop from '../../assets/home/ScrollStop.png';
+import ScrollUp from '../../assets/home/ScrollUp.png';
 
 const HomePage = () => (
   <ReactFullpage
@@ -21,16 +24,21 @@ const HomePage = () => (
           <div className="section home-section1">
             <img className="home-section1-image" src={HomeSection1} />
             <div><Link to="/login" className="home-link text-decoration-none">시작하기</Link></div>
+            <img className="home-scroll-down" src={ScrollDown} />
           </div>
           <div className="section home-section2">
             <img className="home-section2-image" src={HomeSection2} />
+            <img className="home-scroll-down" src={ScrollDown} />
           </div>
           <div className="section home-section3">
            <img className="home-section3-image" src={HomeSection3} />
+           <img className="home-scroll-down" src={ScrollDown} />
           </div>
           <div className="section home-section4">
-            <img className="home-section4-image" src={HomeSection4} />  
+            <img className="home-section4-image" src={HomeSection4} />
+            <img className="home-scroll-down" src={ScrollStop} />
           </div>
+          <img className="home-scroll-up" src={ScrollUp} />
         </ReactFullpage.Wrapper>
       );
     }}
