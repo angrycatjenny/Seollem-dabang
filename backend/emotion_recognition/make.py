a0b159619e68455a315a0fe665cfcea16d3c9494
차종욱 <hedge0207@gmail.com>
2020-11-11 17:54:18 +0900

    com pandas code

--- backend/emotion_recognition/make.py
+++
@@ -1,4 +0,0 @@
-import check_emotion
-
-def get_emotions(FILE_NAME):
-    return check_emotion.get_emotion(FILE_NAME)
--- backend/emotion_recognition/test.py
+++ backend/emotion_recognition/test.py
@@ -1,11 +1,9 @@
 import sys
 from check_emotion import get_emotion
 
-result = get_emotion('1601631973679')
-print(result)
-
 def main(argv):
   result = get_emotion(argv[1])
+  print(result)
 
 if __name__=="__main__":
   main(sys.argv)
