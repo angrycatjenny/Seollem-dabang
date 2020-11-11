f22452fbc08392900d08aea5649f3173c46e1a3d
이한솔 <henryhansollee@gmail.com>
2020-11-11 16:40:46 +0900

    Add street lamp

--- frontend/src/components/base/FooterComp.js
+++ frontend/src/components/base/FooterComp.js
@@ -3,10 +3,12 @@ import './FooterComp.css';
 import FooterPost from '../../assets/footer/FooterPost.png';
 import FooterFlower1 from '../../assets/footer/FooterFlower1.png';
 import FooterFlower2 from '../../assets/footer/FooterFlower2.png';
+import FooterLamp from '../../assets/footer/FooterLamp.png';
 
 export default function FooterComp() {
   return (
     <div className="fixed-bottom">
+      <img className="footer-lamp" src={FooterLamp} />
       <img className="footer-flower1" src={FooterFlower1} />
       <div className="footer-logout d-flex flex-column">
         <small className="ml-2">로그아웃</small>
