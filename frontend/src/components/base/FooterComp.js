ec66016c3266b914bf443c8f9a3e9ecc95062247
이한솔 <henryhansollee@gmail.com>
2020-11-12 01:03:40 +0900

    Update footer left

--- frontend/src/components/base/FooterComp.js
+++ frontend/src/components/base/FooterComp.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import './FooterComp.css';
+import { useCookies } from 'react-cookie';
+import { useHistory } from "react-router-dom";
 import FooterPost from '../../assets/footer/FooterPost.png';
 import FooterFlower1 from '../../assets/footer/FooterFlower1.png';
 import FooterFlower2 from '../../assets/footer/FooterFlower2.png';
 import FooterLamp from '../../assets/footer/FooterLamp.png';
+import './FooterComp.css';
 
-// Cookie
-import { useCookies } from 'react-cookie';
-
-// History
-import { useHistory } from "react-router-dom";
-
-export default function FooterComp() {
+const FooterComp = () => {
   const history = useHistory();
 
   const [cookies, setCookie, removeCookie] = useCookies(['accessToken']);
@@ -26,6 +22,14 @@ export default function FooterComp() {
     <div className="fixed-bottom">
       <img className="footer-lamp" src={FooterLamp} />
       <img className="footer-flower1" src={FooterFlower1} />
+      <div className="footer-left"></div>
+      <div className="footer-right"></div>
+
+
+
+
+
+
       <div className="footer-logout d-flex flex-column">
         <small className="ml-2 logout-button" onClick={logout}>로그아웃</small>
         <div className="d-flex">
@@ -36,4 +40,6 @@ export default function FooterComp() {
       <div className="footer-inner"></div>
     </div>
   );
-}
+};
+
+export default FooterComp;
