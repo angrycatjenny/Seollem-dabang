7032a5395a073cf0664376833377e163d84292c5
김도연 <kdymay93@naver.com>
2020-11-13 03:41:33 +0900

    style mainpage

--- frontend/src/components/recommend/OtherRecommend.js
+++ frontend/src/components/recommend/OtherRecommend.js
@@ -1,35 +1,70 @@
 import React from 'react';
-import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
+import { Modal } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import './OtherRecommend.css';
 import AudioPlayer from 'react-modular-audio-player';
-
-import menImage1 from '../../assets/mainImg/png/남성1.png';
-import menImage2 from '../../assets/mainImg/png/남성2.png';
-import menImage3 from '../../assets/mainImg/png/남성3.png';
-import menImage4 from '../../assets/mainImg/png/남성4.png';
-
-import womenImage1 from '../../assets/mainImg/png/여성1.png';
-import womenImage2 from '../../assets/mainImg/png/여성2.png';
-import womenImage3 from '../../assets/mainImg/png/여성3.png';
-import womenImage4 from '../../assets/mainImg/png/여성4.png';
 import { useHistory } from "react-router-dom";
 import { useCookies } from 'react-cookie';
 import axios from 'axios';
 
-const OtherRecommend = () => {
-  const menImgData = [menImage1, menImage2, menImage3, menImage4]
-  const womenImgData = [womenImage1, womenImage2, womenImage3, womenImage4]
+let rearrangedPlayer = [
+  {
+    className: "adele",
+    innerComponents: [
+      {
+        type: "play",
+        style: {
+          width: "100%",
+          justifyContent: "center",
+          filter: "invert(100%)",
+          opacity: "0.4"
+        }
+      }
+    ]
+  }
+];
+
+function MyVerticallyCenteredModal(props) {
   const history = useHistory();
+  return (
+    <Modal
+      {...props}
+      size="sm"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.data.nickname}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>거주 지역:{props.data.location}</h4>
+        <h4>나이:{props.data.age}</h4>
+      </Modal.Body>
+      <Modal.Footer>
+        <AudioPlayer
+          audioFiles={[
+            {
+              src: `${props.data.voiceDownloadUri}`,
+              title: "voiceDownloadUri",
+            }
+          ]}
+          rearrange={rearrangedPlayer}
+          playerWidth="50px"
+          iconSize="50px"
+        />
+        <Button onClick={() => history.push(`/answer/${props.data.id}`)}>레시피 풀기</Button>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
-  const [gender, setGender] = React.useState();
+const OtherRecommend = () => {
+  const [modalShow, setModalShow] = React.useState(false);
   const [loading, setLoading] = React.useState(false)
-
   const [tileData, setTileData] = React.useState()
-  const [tileData2, setTileData2] = React.useState([])
   const [cookies] = useCookies(['accessToken']);
 
   const axiosConfig = {
@@ -41,7 +76,6 @@ const OtherRecommend = () => {
       .then((response) => {
         console.log('other', response)
         setTileData(response.data)
-        setGender(response.data[0].gender)
         setLoading(true)
       })
       .catch((err) => {
@@ -52,75 +86,30 @@ const OtherRecommend = () => {
     getData()
   }, [])
 
-let tileData3 = []
 
-  if (loading) {
-  for (let i = 0; i<2; i++){
-    tileData3.push(tileData[i])
-  }
-  console.log('push네여ㅕㅕ',tileData3)
-    if (gender === 0) {
-      for (let i = 0; i < tileData.length; i++) {
-        tileData[i].img = menImgData[i]
-      }
-    } else {
-      for (let i = 0; i < tileData.length; i++) {
-        tileData[i].img = womenImgData[i]
-      }
-    }
-  } else {
+  if (!loading) {
     return (
       <h3>서비스 준비 중 입니다.</h3>
     )
   }
-
-  let rearrangedPlayer = [
-    {
-      className: "adele",
-      innerComponents: [
-        {
-          type: "play",
-          style: {
-            width: "100%",
-            justifyContent: "center",
-            filter: "invert(100%)",
-            opacity: "0.4"
-          }
-        }
-      ]
-    }
-  ];
-console.log(456789)
   return (
-    <div className="other-root">
-      <GridList cellHeight={200} className="other-gridlist">
-        {tileData3.map((tile) => (
-          <GridListTile key={tile.img} className="other-gridlist-item">
-            <Button onClick={() => history.push(`/answer/${tile.id}`)}>
-              <img src={tile.img} alt={tile.nickname} className="other-imgsize" />
+    <div>
+      {tileData.map((data) => (
+        <div className="item" key={data.nickname}>
+          <div className="polaroid">
+            <div className="caption">지역/나이 기반 추천</div>
+            <Button onClick={() => setModalShow(true)}>
+              <img src={data.imageDownloadUri} alt={data.nickname} className="wrapper-img" />
             </Button>
-            <GridListTileBar
-              title={tile.name}
-              subtitle={<span>{tile.location}, {tile.age}세</span>}
-              actionIcon={
-                <IconButton className="other-icon">
-                  <AudioPlayer
-                    audioFiles={[
-                      {
-                        src: `${tile.voiceDownloadUri}`,
-                        title: "voiceDownloadUri",
-                      }
-                    ]}
-                    rearrange={rearrangedPlayer}
-                    playerWidth="50px"
-                    iconSize="50px"
-                  />                
-                  </IconButton>
-              }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
+            <div className="caption">{data.nickname}</div>
+          </div>
+          <MyVerticallyCenteredModal
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            data={data}
+          />
+        </div>
+      ))}
     </div>
   );
 };
