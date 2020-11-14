506f35aea678cb0a3bc6d2ca8441f0e3e7110727
최보근 <bogun11904@naver.com>
2020-11-14 22:57:19 +0900

    Create proxy middleware

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,10 @@
+import createProxyMiddleware from 'http-proxy-middleware';
+
+module.exports = (app) => {
+    app.use(
+        createProxyMiddleware({
+            target: 'https://k3b103.p.ssafy.io:8080',
+            changeOrigin: true,
+        })
+    );
+};
