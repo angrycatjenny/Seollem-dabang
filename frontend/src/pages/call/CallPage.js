c3ab3c7d75ad730087a4d2823d1d75dfa93f6267
이한솔 <henryhansollee@gmail.com>
2020-11-14 23:46:10 +0900

    Create call feedback

--- frontend/src/pages/call/CallPage.js
+++ frontend/src/pages/call/CallPage.js
@@ -5,14 +5,21 @@ import { useCookies } from 'react-cookie';
 import axios from 'axios';
 import { useHistory } from "react-router-dom";
 import useMediaRecorder from '@wmik/use-media-recorder';
+import { Link } from 'react-router-dom';
 
 const CallPage = ({ match }) => {
   const [ cookies, setCookie ] = useCookies(['accessToken']);
   const { stream, error } = useUserMediaFromContext();
   const [ user, setUser ] = useState('');
-  const [ feedback, setFeedback ] = useState('');
   const history = useHistory();
-
+  const [ angry, setAngry ] = useState('');
+  const [ disgusted, setDisgusted ] = useState('');
+  const [ fearful, setFearful ] = useState('');
+  const [ happy, setHappy ] = useState('');
+  const [ neutral, setNeutral ] = useState('');
+  const [ sad, setSad ] = useState('');
+  const [ feedback, setFeedback ] = useState('');
+ 
   const config = {
     headers: {
       'Authorization': 'Bearer ' + cookies.accessToken
@@ -47,8 +54,20 @@ const CallPage = ({ match }) => {
     
     axios.post(`/conversation/end/${match.params.conversationId}`, callFile, config)
       .then((response) => {
-        console.log(response)
-        setFeedback(response.data)
+        console.log(response.data)
+        setAngry(response.data.Angry)
+        setDisgusted(response.data.Disgusted)
+        setFearful(response.data.Fearful)
+        setHappy(response.data.Happy)
+        setNeutral(response.data.Neutral)
+        setSad(response.data.Sad)
+        if (response.data.Happy > response.data.Disgusted) {
+          setFeedback('분위기가 아주 좋은데요? 호감을 표현해도 되겠어요!')
+        } else if (response.data.Happy = response.data.Disgusted) {
+          setFeedback('분위기가 나쁘지 않아요! 더욱 적극적으로 다가가 보세요!')
+        } else if (response.data.Happy < response.data.Disgusted) {
+          setFeedback('아직은 때가 아닌가봐요,,, 조금 조심스럽게 다가가 보세요!')
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -74,7 +93,17 @@ const CallPage = ({ match }) => {
         <div>
           {status == 'stopped' &&
             <div>
-              <button onClick={goResult}>결과보기</button>
+              {!feedback &&
+                <div>
+                  <button onClick={goResult}>결과보기</button>
+                </div>
+              }
+              {feedback &&
+                <div>
+                  <h3>{feedback}</h3>
+                  <Link to='/main'>나가기</Link>
+                </div>
+              }
             </div>
           }
         </div>
