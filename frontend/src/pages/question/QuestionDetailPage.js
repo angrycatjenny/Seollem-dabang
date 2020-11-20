c0ba6868cdd2f846219e69d6121c5bf79fab6c22
이한솔 <henryhansollee@gmail.com>
2020-11-20 13:23:22 +0900

    Optimize Question detail

--- frontend/src/pages/question/QuestionDetailPage.js
+++ frontend/src/pages/question/QuestionDetailPage.js
@@ -7,28 +7,20 @@ import { useHistory } from "react-router-dom";
 import './QuestionDetailPage.css';
 import TextField from '@material-ui/core/TextField';
 import Radio from '@material-ui/core/Radio';
-//Footer
 import FooterComp from '../../components/base/FooterComp';
-//완료: 질문 추가, 시험지 전체 삭제, 질문 삭제
-//아직: 질문 수정
+
 const QuestionDetailPage = () => {
     const history = useHistory();
     const [ exam, setExam ] = useState('');
     const [ isExam, setIsExam ] = useState(false);
-
-    //수정 담당 data
     const [ editId, setEditId ] = useState(-1);
     const [ editQuest, setEditQuest ] = useState('');
     const [ editAns, setEditAns ] = useState(null);
-
-    //유저 정보
     const [cookies, setCookie] = useCookies(['accessToken']);
     const config = {
       headers: { 'Authorization':'Bearer '+ cookies.accessToken } 
     }
     const [ nickname, setNickname ] = useState('')
-
-    //새로운 질문 추가
     const [ newQuest, setNewQuest  ] = useState('')
     const [ newAns, setNewAns ] = useState(-1)
 
@@ -65,8 +57,6 @@ const QuestionDetailPage = () => {
             .catch((error) => console.log(error))
       }
     }
-
-  //만들어둔 시험 문제 get
   useEffect(() => {
     setTimeout(() => {
       const fetchData = async() => {
@@ -82,7 +72,6 @@ const QuestionDetailPage = () => {
       };
       fetchData();
     },1000);
-    //유저 정보 가져오기  
     axios.get('/api/my-profile', config)
     .then((response) => {
       setNickname(response.data.nickname)
@@ -96,8 +85,6 @@ const QuestionDetailPage = () => {
       if(!exam){
         return null;
       }
-
-    //시험지 전체 삭제
     const delExam = () => {
         axios.delete('/api/question/delete', config)
         .then(() => {
@@ -106,15 +93,9 @@ const QuestionDetailPage = () => {
         .catch((error) => console.log(error))
     }
 
-    ////질문 개별 수정 및 삭제
-    //수정할 질문 ID 설정
     const sendEditId = (Id) => {
       setEditId(Id);
-    //   const targeting = exam.filter((quest) => {
-    //     return quest.name.indexOf(exam) > -1;
-    // });
     }
-    //질문 수정
     const EditQuestion = e => {
       let edited = e.target.value
       setExam(exam.map((item) =>
@@ -122,7 +103,6 @@ const QuestionDetailPage = () => {
       setEditQuest(edited)
     }
   
-    //정답 수정
     const EditAnswerYes = (Id) => {
       setEditAns(true)
       setExam(exam.map((item) =>
@@ -133,15 +113,11 @@ const QuestionDetailPage = () => {
       setExam(exam.map((item) =>
       item.questionId === Id ? {...item, correctAnswer:false} : item))
     }
-    //수정된 데이터 보내기
     const updateQuest = (Id) => {
-      // console.log(Id,'수정')
-      // console.log(exam,'exam')
       const ExamData = {
         "content": editQuest,
         "correctAnswer": editAns
       }
-      // console.log(ExamData,'보낼거')
       axios.put(`/api/question/update/${Id}`, ExamData, config)
         .then(() => {
             history.push('/question/detail')
@@ -150,12 +126,10 @@ const QuestionDetailPage = () => {
         .catch((error) => console.log(error))
     }
 
-    //삭제
     const deleteQuest = (Id) => {
       if(exam.length>5){
         axios.delete(`/api/question/delete/${Id}`, config)
           .then(() => {
-            //push하니까 안됨
               history.go('/question/detail')
           })
           .catch((error) => console.log(error))
@@ -167,7 +141,6 @@ const QuestionDetailPage = () => {
   return (
     <div style={{display:"flex", justifyContent:"center"}}>
       <div className="quest-detail">
-        {/* 목록으로 돌아가기 & 시험지 통째로 삭제 */}
         <div className="detail-btn-box">
           <div className="detail-btn">
             <Link to="/question">
@@ -287,7 +260,6 @@ const QuestionDetailPage = () => {
             </div>
 
         </div>
-        {/* <FooterComp/> */}
       </div>
     </div>
   );
